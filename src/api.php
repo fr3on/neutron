d0ee72bf3a8b9ae6222776d7e17f25669ad25da0
Pharaoh <hello@fr3on.info>
2018-09-27 14:24:39 +0200

    api.php
    api auth

---
+++ src/api.php
@@ -0,0 +1,48 @@
+<?php
+
+function curl_wrap($entity, $data, $method, $content_type) {
+        if ($content_type == NULL) {
+            $content_type = "application/json";
+        }
+        $aeure_url = "http://insta.fr3on.info/" . $entity;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 100);
+        curl_setopt($ch, CURLOPT_UNRESTRICTED_AUTH, true);
+        switch ($method) {
+            case "POST":
+                $url = $aeure_url;
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                break;
+            case "GET":
+                $url = $aeure_url;
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+                break;
+            case "PUT":
+                $url = $aeure_url;
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                break;
+            case "DELETE":
+                $url = $aeure_url;
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+                break;
+            default:
+                break;
+        }
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Content-type : $content_type;", 'Accept : application/json'
+        ));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 1000);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
+    }
